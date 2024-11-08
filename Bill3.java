31c9e61cc87767503552c1d168405c033255a735
yotam levy <yotam.levy01@post.runi.ac.il>
2024-11-08 11:11:14 +0200

    HW1 completed

--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,17 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
+	    String name1 = args [0];
+		String name2 = args [1];
+		String name3 = args [2];
+		int bill = Integer.parseInt (args [3]);
+		double eachbill = bill;
+		eachbill = Math.ceil (eachbill/3);
+
+		System.out.println ("Dear " + name1 + ", " + name2 + ", and " + name3 + ": pay " + eachbill + " shekels each."); 
+
+
+		
 	    // Replace this comment with the rest of your code   
-	}
+	} 
 }
--- FVCalc.java
+++ FVCalc.java
@@ -2,5 +2,15 @@
 public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
-	}
+		int years = Integer.parseInt(args [2]);
+		double rate = Double.parseDouble(args [1]);
+		int savings = Integer.parseInt(args [0]);
+        double rateperyear = rate/100;
+		double futurevalue = savings * Math.pow(1+rateperyear, years);
+
+		System.out.println ("after " + years + " years, $" + savings + " saved at " + rate + "%" + " will yield $" + (int)futurevalue);
+
+
+
+	} 
 }
